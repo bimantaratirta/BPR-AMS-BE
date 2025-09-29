0df2e7ede633ab128056697af8e3953a75e3a71c
aryafirmansyah2 <aryafirmansyah200404@example.com>
2025-09-29 21:21:04 +0700

    fix doc kar

--- src/domains/KAR/KAR-routes.js
+++ src/domains/KAR/KAR-routes.js
@@ -1,49 +1,40 @@
-import BaseRoutes from "../../base_classes/base-routes.js";
-import tryCatch from "../../utils/tryCatcher.js";
-import validateCredentials from "../../middlewares/validate-credentials-middleware.js";
-import KARController from "./KAR-controller.js";
-import { createKAR } from "./KAR-schema.js";
-import authToken from "../../middlewares/auth-token-middleware.js";
+import BaseRoutes from '../../base_classes/base-routes.js';
+import tryCatch from '../../utils/tryCatcher.js';
+import validateCredentials from '../../middlewares/validate-credentials-middleware.js';
+import KARController from './KAR-controller.js';
+import { createKAR } from './KAR-schema.js';
+import authToken from '../../middlewares/auth-token-middleware.js';
 
 class KARRoutes extends BaseRoutes {
-    routes() {
-        this.router.get("/", [
-            authToken,
-            tryCatch(KARController.get)
-        ])
+  routes() {
+    this.router.get('/', [authToken, tryCatch(KARController.get)]);
 
-        this.router.get("/:id", [
-            authToken,
-            tryCatch(KARController.getById)
-        ])
+    this.router.get('/:id', [authToken, tryCatch(KARController.getById)]);
 
-        this.router.post("/", [
-            authToken,
-            validateCredentials(createKAR),
-            tryCatch(KARController.post)
-        ])
+    this.router.post('/', [
+      authToken,
+      validateCredentials(createKAR),
+      tryCatch(KARController.post),
+    ]);
 
-        this.router.put("/:id", [
-            authToken,
-            validateCredentials(createKAR),
-            tryCatch(KARController.put)
-        ])
+    this.router.put('/:id', [
+      authToken,
+      validateCredentials(createKAR),
+      tryCatch(KARController.put),
+    ]);
 
-        this.router.delete("/:id", [
-            authToken,
-            tryCatch(KARController.deleteById)
-        ])
+    this.router.delete('/:id', [authToken, tryCatch(KARController.deleteById)]);
 
-        this.router.get('/:id/generate-pdf', [
-            authToken,
-          tryCatch(KARController.generatePDF)
-        ]);
+    this.router.get('/:id/generate-pdf', [
+      // authToken,
+      tryCatch(KARController.generatePDF),
+    ]);
 
-        this.router.get('/:id/generate-docx', [
-            authToken,
-            tryCatch(KARController.generateDOCX)
-        ]);
-    }
+    this.router.get('/:id/generate-docx', [
+      authToken,
+      tryCatch(KARController.generateDOCX),
+    ]);
+  }
 }
 
 export default new KARRoutes().router;
